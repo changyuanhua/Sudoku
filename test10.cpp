2e0e170a300d613cec8517f12a5674ee0b8d9bc8
changyuanhua <chang19960726@gmail.com>
2016-04-02 21:01:00 +0800

    OK

--- test10.cpp
+++ test10.cpp
@@ -2,18 +2,20 @@
 #include <fstream>
 #include <iomanip>
 #include <set>
-
+int print(int A[][9]);
+int p=0;
+int B[9][9];
 using namespace std;
 bool check(int A[][9], int nextblank, int num)
 {
-    int i, j;
+    int i;
 	int col=nextblank%9;
 	int row=nextblank/9;
     for(i=0;i<9;i++)
 	    if(A[row][i]==num||A[i][col]==num)
 		    return false;	
 	for(i=0;i<3;i++)
-		if(A[(row/3)*3][i+(col/3)*3]||A[i+(row/3)*3][(col/3)*3])
+		if(A[(row/3)*3][i+(col/3)*3]==num||A[i+(row/3)*3][(col/3)*3]==num)
 				return false;
 	return true;
 }
@@ -27,30 +29,23 @@ int findblank(int A[][9],int blank)
 	}
     return 81;
 }	
-int print(int A[][9]);
 int solve(int A[][9],int blank)
 {
 	int nextblank=findblank(A,blank);
 	int i,j,num;
+	if(p>1)
+		return 0;
 	if(nextblank==81)
 	{
-		    for (i=0; i<9; ++i) 
-			for(j=0;j<9;j++)
-			{
-				      printf("%d ", A[i][j]);
-					        if (j==8) printf("\n");
-							    }
-			    printf("\n");
-				    return 0;
-					  }
-	for(num=1;num<=10;num++)
+		p++;
+		for (i=0; i<9; i++)
+			for (j=0; j<9; j++)
+				B[i][j]=A[i][j];
+	    return 0;
+		
+	}
+	for(num=1;num<=9;num++)
 	{
-		if(num>9)
-		{
-			A[nextblank/9][nextblank%9]=0;
-		}
-		else
-		{
 			if(check(A,nextblank,num))
 			{
 				A[nextblank/9][nextblank%9]=num;
@@ -58,31 +53,41 @@ int solve(int A[][9],int blank)
 					return 1;
 				A[nextblank/9][nextblank%9]=0;
 			}
-		}
+		
 	}
 	return 0;
 }
 int print(int A[][9])
 {
     int i,j;
-    for (i=0; i<9; i++)
+	cout << p << endl;
+    if(p==0)
+		return 0;
+	for (i=0; i<9; i++)
 	    for(j=0; j<9; j++)
-         	printf("%d%c ", A[i][j],(j+1)%9==0?'\n':' ');
+		{
+         	if(j!=8)
+				      cout<<" "<< A[i][j] ;
+	        else
+			          cout<<" "<< A[i][j]<<endl;
+		}
+	
 	return 0;
 }
 int main()
 {
 	int i,j;		     
-   	int A[9][9]={{0,2,1,0,6,0,5,9,0},			
-		{0,0,0,0,0,0,0,0,2},
-		{4,0,0,2,0,5,0,6,3},
-		{3,0,7,6,0,0,0,0,0},
-		{0,9,5,0,0,0,3,2,0},
-		{0,0,0,0,0,3,7,0,1},
-		{9,3,0,4,0,2,0,0,7},
-		{5,0,0,0,0,0,0,0,0},
-		{0,1,2,0,9,0,0,3,0}};
+   	int A[9][9]={{2,3,1,9,0,6,7,8,0},
+		{0,8,6,7,5,0,2,3,1},
+		{7,0,0,1,2,3,0,0,6},
+		{3,2,8,0,6,7,1,0,5},
+		{5,6,7,8,1,0,3,2,0},
+		{0,1,0,2,3,0,8,6,7},
+		{1,0,2,6,0,5,0,7,3},
+		{6,7,3,0,8,2,5,1,0},
+		{0,5,0,3,7,1,6,0,2}};
 	solve(A,0);
+	print(B);
 	return 0;
 }
 
